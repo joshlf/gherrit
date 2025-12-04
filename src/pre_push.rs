5c4ec5c04b6d20590533d93ccb251a42dfe0f16a
Joshua Liebow-Feeser <hello@joshlf.com>
2025-12-03 21:14:09 -0500

    [pre_push] Refactor to avoid mega-function
    gherrit-pr-id: Gca25233de674adafb9794102f4fe491ae680e842

--- src/pre_push.rs
+++ src/pre_push.rs
@@ -21,9 +21,32 @@ pub fn run() {
     let (repo, branch_name) =
         util::get_current_branch().unwrap_or_exit("Failed to get current branch");
 
-    // Step 1: Resolve State
+    check_managed_state(&repo, &branch_name);
+
+    let commits = collect_commits(&repo).unwrap_or_exit("Failed to collect commits");
+
+    let t1 = Instant::now();
+    log::trace!("t0 -> t1: {:?}", t1 - t0);
+
+    let commits = create_gherrit_refs(&repo, commits).unwrap_or_exit("Failed to create refs");
+
+    let t2 = Instant::now();
+    log::trace!("t1 -> t2: {:?}", t2 - t1);
+
+    if commits.is_empty() {
+        log::info!("No commits to sync.");
+        return;
+    }
+
+    push_to_origin(&repo, &branch_name, &commits);
+
+    sync_prs(&repo, &branch_name, commits);
+}
+
+// TODO: Maybe this should return a Result instead of bailing from inside?
+fn check_managed_state(repo: &gix::Repository, branch_name: &str) {
     let state =
-        manage::get_state(&repo, &branch_name).unwrap_or_exit("Failed to parse gherritManaged");
+        manage::get_state(repo, branch_name).unwrap_or_exit("Failed to parse gherritManaged");
 
     match state {
         Some(manage::State::Unmanaged) => {
@@ -31,7 +54,7 @@ pub fn run() {
                 "Branch '{}' is UNMANAGED. Allowing standard push.",
                 branch_name
             );
-            return; // Allow standard push
+            std::process::exit(0); // Allow standard push
         }
         Some(manage::State::Managed) => {
             log::info!("Branch '{}' is MANAGED. Syncing stack...", branch_name);
@@ -46,54 +69,44 @@ pub fn run() {
             std::process::exit(1);
         }
     }
+}
 
-    let head = repo.rev_parse_single("HEAD").unwrap();
-    let main = repo.rev_parse_single("main").unwrap();
+fn collect_commits(repo: &gix::Repository) -> Result<Vec<Commit>, Box<dyn Error>> {
+    let head = repo.rev_parse_single("HEAD")?;
+    let main = repo.rev_parse_single("main")?;
     let mut commits = repo
         .rev_walk([head])
-        .all()
-        .unwrap()
+        .all()?
         .take_while(|res| res.as_ref().map(|info| info.id != main).unwrap_or(true))
         .map(|res| -> Result<_, Box<dyn Error>> {
             res.map_err::<Box<dyn Error>, _>(|e| Box::new(e))
                 .and_then(|info| info.object().map_err::<Box<dyn Error>, _>(|e| Box::new(e)))
         })
-        .try_collect::<Vec<_>>()
-        .unwrap();
+        .try_collect::<Vec<_>>()?;
     commits.reverse();
 
-    let t1 = Instant::now();
-    log::trace!("t0 -> t1: {:?}", t1 - t0);
-
-    let commits = commits
-        .iter()
-        .map(Commit::try_from_gix)
+    commits
+        .into_iter()
+        .map(|c| c.try_into())
         .try_collect::<Vec<_>>()
-        .unwrap();
+}
 
-    let t2 = Instant::now();
-    log::trace!("t1 -> t2: {:?}", t2 - t1);
-
-    let commits = commits
+fn create_gherrit_refs(
+    repo: &gix::Repository,
+    commits: Vec<Commit>,
+) -> Result<Vec<Commit>, Box<dyn Error>> {
+    commits
         .into_iter()
         .map(|c| -> Result<_, Box<dyn Error>> {
-            let gherrit_id = c.gherrit_id;
-            let rf = format!("refs/gherrit/{gherrit_id}");
+            let rf = format!("refs/gherrit/{}", c.gherrit_id);
             let _ = repo.reference(rf, c.id, PreviousValue::Any, "")?;
-            Ok((c, gherrit_id))
+            Ok(c)
         })
         .try_collect::<Vec<_>>()
-        .unwrap();
+}
 
-    let t3 = Instant::now();
-    log::trace!("t2 -> t3: {:?}", t3 - t2);
-
-    if commits.is_empty() {
-        log::info!("No commits to sync.");
-        return;
-    }
-
-    let is_private = util::get_config_bool(&repo, &format!("branch.{branch_name}.gherritPrivate"))
+fn push_to_origin(repo: &gix::Repository, branch_name: &str, commits: &[Commit]) {
+    let _is_private = util::get_config_bool(repo, &format!("branch.{branch_name}.gherritPrivate"))
         .unwrap_or_exit("Failed to read config")
         .unwrap_or(true);
 
@@ -109,7 +122,7 @@ pub fn run() {
     args.extend(
         commits
             .iter()
-            .map(|(c, gherrit_id)| format!("{}:refs/heads/{gherrit_id}", c.id)),
+            .map(|c| format!("{}:refs/heads/{}", c.id, c.gherrit_id)),
     );
 
     log::info!("Pushing {} commits to origin...", commits.len());
@@ -119,14 +132,7 @@ pub fn run() {
         .spawn()
         .unwrap();
 
-    // Filter out the "Create a pull request" message from GitHub:
-    //
-    //   remote:
-    //   remote: Create a pull request for 'G7a4e64a53733779e8f32b7258d5083e5b15ea91d' on GitHub by visiting:
-    //   remote:      https://github.com/joshlf/gherrit/pull/new/G7a4e64a53733779e8f32b7258d5083e5b15ea91d
-    //   remote:
-    //
-    // We use a multi-line regex to match this block specifically.
+    // Filter out the "Create a pull request" message from GitHub
     {
         use std::io::{BufRead, BufReader};
         let stderr = child.stderr.take().unwrap();
@@ -168,11 +174,10 @@ pub fn run() {
         log::error!("`git push` failed. You may need to `git pull --rebase`.");
         std::process::exit(1);
     }
+}
 
+fn sync_prs(repo: &gix::Repository, branch_name: &str, commits: Vec<Commit>) {
     let pr_list = cmd!("gh pr list --json number,headRefName,url").unwrap_output();
-
-    let t4 = Instant::now();
-    log::trace!("t3 -> t4: {:?}", t4 - t3);
 
     #[derive(Serialize, Deserialize, Debug)]
     #[serde(rename_all = "camelCase")]
@@ -199,31 +204,30 @@ pub fn run() {
 
     let commits = commits
         .into_iter()
-        .scan("main", |parent_branch, (c, gherrit_id)| {
-            let parent = *parent_branch;
-            *parent_branch = gherrit_id;
-            Some((c, parent, gherrit_id))
+        .scan("main".to_string(), |parent_branch, c| {
+            let parent = parent_branch.clone();
+            *parent_branch = c.gherrit_id.clone();
+            Some((c, parent))
         })
         .collect::<Vec<_>>();
 
     let commits = commits
         .into_par_iter()
         .map(
-            move |(c, parent_branch, gherrit_id)| -> Result<_, Box<dyn Error + Send + Sync>> {
-                let pr_info = prs.iter().find(|pr| pr.head_ref_name == gherrit_id);
+            move |(c, parent_branch)| -> Result<_, Box<dyn Error + Send + Sync>> {
+                let pr_info = prs.iter().find(|pr| pr.head_ref_name == c.gherrit_id);
 
                 let (pr_num, pr_url) = if let Some(pr) = pr_info {
-                    log::debug!("Found existing PR #{} for {}", pr.number, gherrit_id);
+                    log::debug!("Found existing PR #{} for {}", pr.number, c.gherrit_id);
                     (pr.number, pr.url.clone())
                 } else {
-                    log::debug!("No GitHub PR exists for {gherrit_id}; creating one...");
-                    // Note that the PR's body will soon be overwritten
-                    // (when we add the Markdown links to other PRs).
-                    // However, setting a reasonable default PR body makes
-                    // sense here in case something crashes between here and
-                    // there.
-                    let (num, url) =
-                        create_gh_pr(parent_branch, gherrit_id, c.message_title, c.message_body)?;
+                    log::debug!("No GitHub PR exists for {}; creating one...", c.gherrit_id);
+                    let (num, url) = create_gh_pr(
+                        &parent_branch,
+                        &c.gherrit_id,
+                        &c.message_title,
+                        &c.message_body,
+                    )?;
 
                     log::info!("Created PR #{num}: {url}");
                     (num, url)
@@ -237,8 +241,9 @@ pub fn run() {
         .try_collect::<Vec<_>>()
         .unwrap();
 
-    let t5 = Instant::now();
-    log::trace!("t4 -> t5: {:?}", t5 - t4);
+    let is_private = util::get_config_bool(repo, &format!("branch.{branch_name}.gherritPrivate"))
+        .unwrap_or_exit("Failed to read config")
+        .unwrap_or(true);
 
     // Attempt to resolve `HEAD` to a branch name so that we can refer to it
     // in PR bodies. If we can't, then silently fail and just don't include
@@ -278,16 +283,16 @@ pub fn run() {
 
                 // Determine parent and child IDs, which may be `None` at the
                 // beginning or end of the list.
-                let parent_gherrit_id = (i > 0).then(|| commits[i - 1].0.gherrit_id);
-                let child_gherrit_id = (i < commits.len() - 1).then(|| commits[i + 1].0.gherrit_id);
+                let parent_gherrit_id = (i > 0).then(|| commits[i - 1].0.gherrit_id.clone());
+                let child_gherrit_id = (i < commits.len() - 1).then(|| commits[i + 1].0.gherrit_id.clone());
 
-                let current_gherrit_id = c.gherrit_id;
-                let message_title = &c.message_title;
-                let message_body = &c.message_body;
+                let current_gherrit_id = c.gherrit_id.clone();
+                let message_title = c.message_title.clone();
+                let message_body = c.message_body.clone();
 
                 s.spawn(move || -> Result<(), std::io::Error> {
                     let re = gherrit_pr_id_re();
-                    let body = re.replace(message_body, "");
+                    let body = re.replace(&message_body, "");
 
                     // 3. Generate Metadata JSON
                     // We generate a JSON object stored in an HTML comment.
@@ -314,7 +319,7 @@ pub fn run() {
 
                     log::debug!("Updating PR #{} description...", pr_num);
                     log::info!("Updated PR #{}: {}", pr_num, pr_url);
-                    edit_gh_pr(*pr_num, parent_branch, message_title, &body)?;
+                    edit_gh_pr(*pr_num, parent_branch, &message_title, &body)?;
                     Ok(())
                 })
             })
@@ -323,9 +328,6 @@ pub fn run() {
         for handle in join_handles {
             let _: () = handle.join().unwrap()?;
         }
-
-        let t6 = Instant::now();
-        log::trace!("t5 -> t6: {:?}", t6 - t5);
 
         Ok(())
     })
@@ -350,27 +352,30 @@ pub fn run() {
     }
 }
 
-struct Commit<'a> {
+struct Commit {
     id: ObjectId,
-    gherrit_id: &'a str,
-    message_title: &'a str,
-    message_body: &'a str,
+    gherrit_id: String,
+    message_title: String,
+    message_body: String,
 }
 
-impl<'a> Commit<'a> {
-    fn try_from_gix(c: &'a gix::Commit) -> Result<Commit<'a>, Box<dyn Error>> {
+impl<'a> TryFrom<gix::Commit<'a>> for Commit {
+    type Error = Box<dyn Error>;
+
+    fn try_from(c: gix::Commit) -> Result<Self, Self::Error> {
         let message = c.message()?;
-        let message_title = core::str::from_utf8(message.title)?;
+        let message_title = core::str::from_utf8(message.title)?.to_string();
         let message_body = message
             .body
             .map(|body| core::str::from_utf8(body).unwrap())
-            .unwrap_or("");
+            .unwrap_or("")
+            .to_string();
         let gherrit_id = {
             let re = gherrit_pr_id_re();
             let captures = re
-                .captures(message_body)
+                .captures(&message_body)
                 .ok_or_else(|| format!("Commit {} missing gherrit-pr-id trailer", c.id))?;
-            let gherrit_id = captures.get(1).unwrap().as_str();
+            let gherrit_id = captures.get(1).unwrap().as_str().to_string();
             gherrit_id
         };
 
